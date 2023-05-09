79c0806467b42542c31662e431dfe71192658041
Arman <armanjafarnejad@gmail.com>
2023-05-09 16:43:11 +0330

    Update manufacturer_data_beacon.py

--- examples/manufacturer_data_beacon.py
+++ examples/manufacturer_data_beacon.py
@@ -4,11 +4,8 @@ from bluezero import broadcaster
 def main():
     alt = broadcaster.Beacon()
     alt.add_manufacturer_data(
-        'ffff',  # Manufacturer ID (0xffff = Not for production)
-        b'\xBE\xAC'   # beacon code for Alt Beacon
-        + 16 * b'\xbe'   # Beacon UUID
-        + 4 * b'\x00'  # Free
-        + b'\x01')  # Transmit power
+        '5558',  # Manufacturer ID (0xffff = Not for production)
+        '18F90849B2CE2C5A7AD4D1494B757CDB7F10111213141516171819')
 
     alt.start_beacon()
 
