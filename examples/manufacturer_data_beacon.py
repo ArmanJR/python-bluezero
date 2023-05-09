82308f1aca244801553d8e9b556a2a3b523fca1e
Arman <armanjafarnejad@gmail.com>
2023-05-09 16:48:45 +0330

    Update manufacturer_data_beacon.py

--- examples/manufacturer_data_beacon.py
+++ examples/manufacturer_data_beacon.py
@@ -5,7 +5,8 @@ def main():
     alt = broadcaster.Beacon()
     alt.add_manufacturer_data(
         '5558',  # Manufacturer ID (0xffff = Not for production)
-        '18F90849B2CE2C5A7AD4D1494B757CDB7F10111213141516171819')
+        bytes.fromhex('18F90849B2CE2C5A7AD4D1494B757CDB7F10111213141516171819')
+    )
 
     alt.start_beacon()
 
